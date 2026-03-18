f2ae16a47abaee407bb8f9b3b996672d8545c6e6
Troit-ui <iessietepalmasIsmael@gmail.com>
2026-03-18 09:26:49 +0000

    Add files via upload

---
+++ ismael_rodrguez_AD51_primerScript/java.js
@@ -0,0 +1,19 @@
+//Esto es el mensaje que sale nada más entrar en la página//
+alert("JavaScript es el lenguaje de programación de la web. Es el pegamento que conecta las aplicaciones y sitios web, y permite que el internet funcione de la manera en que lo conocemos hoy. Brendan Eich.");
+//Definimos las dos variables para hacer una suma//
+let a = 10;
+let b = 85.6;
+
+//Esto es sólo para mostrar los valores de las variables//
+console.log("a =",a);
+console.log("b =",b);
+
+//Lo siguiente es para hacer una suma entre las dos variables mostradas//
+let suma = a+b;
+console.log("La suma es: ",suma);
+
+//Nombramos una constante//
+const NOMBRE = "ISMAEL_RODRIGUEZ_RODRIGUEZ";
+
+//Este comando está en desuso pero se puede hacer igual//
+document.write("<h2>"+ NOMBRE+ "</h2>");
