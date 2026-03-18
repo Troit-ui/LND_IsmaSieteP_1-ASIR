698e1403310b361c447f723704d64a4e1c9af4ba
Troit-ui <iessietepalmasIsmael@gmail.com>
2026-03-18 09:28:24 +0000

    Add files via upload

---
+++ ismael_rodrigue_AD521-variables/script.js
@@ -0,0 +1,18 @@
+//Este es el contacto 1
+let nombreMaxwell = "Maxwell Wright"
+let telefonoMaxwell = "(0191)7196495"
+let emailMaxwell = "Curabitur.egestas.nunc@nonummyac.co.uk"
+//Este es el contacto 2
+let nombreRaja = "Raja Villarreal"
+let telefonoRaja = "08663982895"
+let emailRaja = "posuere.vulputate@SVGFEDropShadowElement.com"
+//Este es el contacto 3
+let nombreHelen = "Helen Richards"
+let telefonoHelen = "08001111"
+let emailHelen = "libero@convallis.edu"
+
+//Vamos a mostrar por consolo el contacto 1 y 3
+
+console.log("nombre:", nombreMaxwell, "telefono:", telefonoMaxwell, "email:", emailMaxwell)
+
+console.log("nombre:", nombreHelen, "telefono:", telefonoHelen, "email:", emailHelen)
